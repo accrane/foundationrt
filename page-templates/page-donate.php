0891453d5c3ca726ce9520f8194e1cd25839f19b
Austin Crane <austincrane@gmail.com>
2016-03-02 10:55:33 -0500

    change content

--- page-templates/page-donate.php
+++ page-templates/page-donate.php
@@ -22,7 +22,7 @@ get_header(); ?>
 					</header><!-- .entry-header -->
 
 					<div class="entry-content">
-						<?php the_content();?>
+						<?php the_field('content');?>
 
 
 						<div class="seminars">
