2db90b05317f2ada382e8be62ec2d02eacecadce
Austin Crane <austincrane@gmail.com>
2017-06-30 14:01:34 -0400

    remove MC signup

--- footer.php
+++ footer.php
@@ -29,44 +29,7 @@ $sitemap = get_field('sitemap_link', 'option');
 						<a target="_blank" href="http://visitor.constantcontact.com/manage/optin?v=001l-FZeZKC-P-q2RjvRzQDDU_YVKo1BEzuNKSfXZAMPtw35Uu4apkgmeC7ssdGmwy73dyKIiC12xGseaUXg5ClE38rwHG0w4mtsfbtBqViXrE%3D">Join Our Mailing List</a>
 					</h3>
 
-					<!--Begin CTCT Sign-Up Form-->
-					<!-- EFD 1.0.0 [Thu Mar 03 10:51:42 EST 2016] -->
 					
-					       <span id="success_message" style="display:none;">
-					           <div style="text-align:center;">Thanks for signing up!</div>
-					       </span>
-					       <form data-id="embedded_signup:form" class="ctct-custom-form Form" name="embedded_signup" method="POST" action="https://visitor2.constantcontact.com/api/signup">
-					           
-					           <!-- The following code must be included to ensure your sign-up form works properly. -->
-					           <input data-id="ca:input" type="hidden" name="ca" value="243494f4-ae9b-434b-96c5-8995410dc30d">
-					           <input data-id="list:input" type="hidden" name="list" value="1">
-					           <input data-id="source:input" type="hidden" name="source" value="EFD">
-					           <input data-id="required:input" type="hidden" name="required" value="list,email">
-					           <input data-id="url:input" type="hidden" name="url" value="">
-					           <input class="footer-email-input" data-id="Email Address:input" type="text" name="email" value="Enter your email..." maxlength="80">
-					           <!-- <p data-id="First Name:p" ><label data-id="First Name:label" data-name="first_name">First Name</label> <input data-id="First Name:input" type="text" name="first_name" value="" maxlength="50"></p> -->
-					           <button type="submit" class="Button ctct-button Button--block Button-secondary" data-enabled="enabled">Join</button>
-					       	<!-- <div><p class="ctct-form-footer">By submitting this form, you are granting: Foundation for Reformed Theology, 4103 Monument Avenue, Richmond, Virginia, 23230-3818, United States, http://www.foundationrt.org permission to email you. You may unsubscribe via the link found at the bottom of every email.  (See our <a href="http://www.constantcontact.com/legal/privacy-statement" target="_blank">Email Privacy Policy</a> for details.) Emails are serviced by Constant Contact.</p></div> -->
-					       </form>
-					   
-					<script type='text/javascript'>
-					   var localizedErrMap = {};
-					   localizedErrMap['required'] = 		'This field is required.';
-					   localizedErrMap['ca'] = 			'An unexpected error occurred while attempting to send email.';
-					   localizedErrMap['email'] = 			'Please enter your email address in name@email.com format.';
-					   localizedErrMap['birthday'] = 		'Please enter birthday in MM/DD format.';
-					   localizedErrMap['anniversary'] = 	'Please enter anniversary in MM/DD/YYYY format.';
-					   localizedErrMap['custom_date'] = 	'Please enter this date in MM/DD/YYYY format.';
-					   localizedErrMap['list'] = 			'Please select at least one email list.';
-					   localizedErrMap['generic'] = 		'This field is invalid.';
-					   localizedErrMap['shared'] = 		'Sorry, we could not complete your sign-up. Please contact us to resolve this.';
-					   localizedErrMap['state_mismatch'] = 'Mismatched State/Province and Country.';
-						localizedErrMap['state_province'] = 'Select a state/province';
-					   localizedErrMap['selectcountry'] = 	'Select a country';
-					   var postURL = 'https://visitor2.constantcontact.com/api/signup';
-					</script>
-					<script type='text/javascript' src='https://static.ctctcdn.com/h/contacts-embedded-signup-assets/1.0.2/js/signup-form.js'></script>
-					<!--End CTCT Sign-Up Form-->
 
 				</div><!-- footer-email -->
 
